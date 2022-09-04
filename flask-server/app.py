6306d60a5c133050831d9dfbf7c272cd07ca9804
JakeBaumbaugh <jake.baumbaugh@gmail.com>
2022-09-04 10:26:39 -0400

    api route returns json string data

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/api/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return jsonify("200 OK")
