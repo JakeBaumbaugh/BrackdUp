dabd438ab173dbab312c1a6d123991eac802c3fc
JakeBaumbaugh <jake.baumbaugh@gmail.com>
2022-09-11 23:31:14 -0400

    finish inserting songs to database

--- flask-server/db/sample-christmas-2021.sql
+++ flask-server/db/sample-christmas-2021.sql
@@ -54,14 +54,78 @@ INSERT INTO song (title, artist, spotify)
 INSERT INTO song (title, artist, spotify)
     VALUES ("Do You Hear What I Hear?", "Whitney Houston", "https://open.spotify.com/track/5umJVEAPT2SocCoB99ZoaH?si=37609f9db59644cb");
 INSERT INTO song (title, artist, spotify)
-    VALUES ("", "", "");
+    VALUES ("Baby It's Cold Outside", "Idina Menzel, Michael Bublé", "https://open.spotify.com/track/0Ie5uiv54KgCr7P4sYDTHl?si=c999415dca9b46d8");
 INSERT INTO song (title, artist, spotify)
-    VALUES ("", "", "");
+    VALUES ("Jingle Bell Rock - Daryl's Version", "Daryl Hall & John Oates", "https://open.spotify.com/track/6pVW5LRWgeLaHudxauOTJU?si=f6a926d551fe4b21");
 INSERT INTO song (title, artist, spotify)
-    VALUES ("", "", "");
+    VALUES ("Christmas (Baby Please Come Home)", "Darlene Love", "https://open.spotify.com/track/46pF1zFimM582ss1PrMy68?si=1f6514eff5754728");
 INSERT INTO song (title, artist, spotify)
-    VALUES ("", "", "");
+    VALUES ("Merry Christmas, Happy Holidays", "*NSYNC", "https://open.spotify.com/track/4v9WbaxW8HdjqfUiWYWsII?si=ee56f75bff5e4a45");
 INSERT INTO song (title, artist, spotify)
-    VALUES ("", "", "");
+    VALUES ("Christmas Every Day", "David Archuleta", "https://open.spotify.com/track/6yOzaZiBk4k4jSOYZcp4pm?si=a88ed855f88e46cb");
 INSERT INTO song (title, artist, spotify)
-    VALUES ("", "", "");
+    VALUES ("Little Drummer Boy", "Pentatonix", "https://open.spotify.com/track/7BvCDrlYwJ5D9RUqvvs3BT?si=702181506efb43bf");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("All I Want for Christmas Is You", "Mariah Carey", "https://open.spotify.com/track/0bYg9bo50gSsH3LtXe2SQn?si=ff310a2bdc6241f8");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Feliz Navidad", "The Last Bandoleros", "https://open.spotify.com/track/02l2uupT1Hf2gRntikMh1k?si=d7140863e7b144d0");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Little Saint Nick", "The Beach Boys", "https://open.spotify.com/track/75dfH68JDisE8dDaD4KlVY?si=7b18989fe2dc425a");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("First Christmas (That I Loved You)", "Shameik Moore", "https://open.spotify.com/track/5GEVVAnsDePwDMHKDunpda?si=2e2e2ad2191f406a");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("White Christmas", "Bing Crosby", "https://open.spotify.com/track/3XsaSIPWvM61RIFfeb0BBR?si=90311fa05ba948a9");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Have Yourself A Merry Little Christmas", "Sam Smith", "https://open.spotify.com/track/1Qi2wh8fFgDV7tl4Sj3f2K?si=6e80196e58054beb");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Mistletoe", "Justin Bieber", "https://open.spotify.com/track/7xapw9Oy21WpfEcib2ErSA?si=1605ba00bc6044f6");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Christmas Wrapping", "The Waitresses", "https://open.spotify.com/track/3nhzTOc939C4v4ecTEZTPl?si=2d872b8fb21f4691");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("It's the Most Wonderful Time of the Year", "Andy Williams", "https://open.spotify.com/track/5hslUAKq9I9CG2bAulFkHN?si=b36fcf4727114c55");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("You're a Mean One, Mr. Grinch", "Thurl Ravenscroft", "https://open.spotify.com/track/6ZgigeSB0XUMqc0jjzaq6d?si=9e84abe81fc54145");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Winter Wonderland", "Leona Lewis", "https://open.spotify.com/track/1YBItUnKj01KBXhpFtvINp?si=e50c797966824aec");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Santy Baby", "Eartha Kitt", "https://open.spotify.com/track/1foCxQtxBweJtZmdxhEHVO?si=ad425c8494bd40c9");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Mary, Did You Know?", "Pentatonix", "https://open.spotify.com/track/4z8sz6E4YyFuEkv5o7IJni?si=a2ea0efe6bdc4c25");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Happy Holiday / The Holiday Season", "Andy Williams", "https://open.spotify.com/track/3sDdyBHQ60Cs1opmIyRvhp?si=32fd32c250624173");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Underneath the Tree", "Kelly Clarkson", "https://open.spotify.com/track/3YZE5qDV7u1ZD1gZc47ZeR?si=67c9b2d05316459f");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Baby, Its Cold Outside", "Ludwig Ahgren, QTCinderella", "https://open.spotify.com/track/6hQ0Cg9t7036jdy5wn0gmm?si=bffd647efad946f8");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Christmas Eve", "Kelly Clarkson", "https://open.spotify.com/track/3JKvHtrY7PXO7afJ9m6IG0?si=630863008939416f");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Last Christmas", "Wham!", "https://open.spotify.com/track/2FRnf9qhLbvw8fu4IBXx78?si=af5b001ecc794018");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Step Into Christmas", "Elton John", "https://open.spotify.com/track/6sBWmE23q6xQHlnEZ8jYPT?si=0daedabe0bcd4098");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Santa Claus Is Comin' to Town", "Bruce Springsteen", "https://open.spotify.com/track/6s2wpWPFPAgKg2LXxi1Oee?si=b872be69af1f4380");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Jingle Bells", "Frank Sinatra", "https://open.spotify.com/track/4KV9bM7a1KDc7b7OakFZic?si=ff145a06dade4129");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Silver Bells", "Dean Martin", "https://open.spotify.com/track/4QX5pZQpQTgVlkqfUTDim0?si=18f1b15855d54874");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Feliz Navidad", "José Feliciano", "https://open.spotify.com/track/0oPdaY4dXtc3ZsaG17V972?si=987afec17cb741e1");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Winter Wonderland / Don't Worry Be Happy", "Pentatonix, Tori Kelly", "https://open.spotify.com/track/2kPpxNSgXRvu5yqkRLKJIu?si=09c83bc905054cc2");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Sleigh Ride", "The Ronettes", "https://open.spotify.com/track/5ASM6Qjiav2xPe7gRkQMsQ?si=b6e72dc9c53a4dba");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Frosty the Snowman", "The Ronettes", "https://open.spotify.com/track/7n5m8nDAnyXo81tr4B3Bcw?si=68675ed209124360");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("What Christmas Means to Me", "CeeLo Green", "https://open.spotify.com/track/2oTH6X8DjSwmTBoAolDou2?si=0ef6d72eae314bb5");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Up On The Housetop", "Pentatonix", "https://open.spotify.com/track/6k0XA5TEtAaWccA3NniAlh?si=3099021adbd14f1a");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("That's Christmas to Me", "Pentatonix", "https://open.spotify.com/track/2U9kDk5mlHYunC7PvbZ8KX?si=87a446d521544564");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("A Mad Russian's Christmas", "Trans-Siberian Orchestra", "https://open.spotify.com/track/4yrvnhXDys0DTs2yTqpLBJ?si=c5d57fba5733418d");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("I Want a Hippopotamus for Christmas", "Gayla Peevey", "https://open.spotify.com/track/5P8Xvy5tzhmfyfA6GplQ8h?si=0adf011c1ab241f0");
+INSERT INTO song (title, artist, spotify)
+    VALUES ("Merry Christmas Everyone", "Shakin' Stevens", "https://open.spotify.com/track/2TE4xW3ImvpltVU0cPcKUn?si=a8ade4af55b1438a");
