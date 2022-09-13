a02b218a0bbcfcc0f9ae15b5f68402523a446433
JakeBaumbaugh <56087211+JakeBaumbaugh@users.noreply.github.com>
2022-09-12 21:39:25 -0400

    2 initial tournament database scripts (#5)
    * first attempt at db schema
    
    * setup db with flask and fixed schema script errors
    
    * fix pylint issues
    
    * attempt to fix pylint action
    
    * fix py error

---
+++ flask-server/db.py
@@ -0,0 +1,38 @@
+"""Database functions"""
+import os
+import sqlite3
+
+import click
+from flask import current_app, g
+
+
+def get_db():
+    """Retrieve the database"""
+    if 'db' not in g:
+        print(current_app.config['DATABASE'])
+        g.db = sqlite3.connect(current_app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(_=None):
+    """Close the database"""
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+@click.command('init-db')
+def init_db_command():
+    """Initialize the database, run with `flask init-db` in terminal"""
+    db = get_db()
+    path = os.path.join('db', 'tournament-schema.sql')
+    with current_app.open_resource(path) as file:
+        db.executescript(file.read().decode('utf8'))
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    """Add initialize and close database functions to the app"""
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+ 
---
+++ flask-server/db/tournament-schema.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS song;
+DROP TABLE IF EXISTS tournament;
+DROP TABLE IF EXISTS tournament_phase;
+DROP TABLE IF EXISTS tournament_matchup;
+
+CREATE TABLE song (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    spotify TEXT,
+    youtube TEXT
+);
+
+CREATE TABLE tournament (
+    id INTEGER PRIMARY KEY AUTOINCREMENT
+);
+
+CREATE TABLE tournament_phase (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tournament_id INTEGER NOT NULL,
+    FOREIGN KEY (tournament_id) REFERENCES tournament (id)
+);
+
+CREATE TABLE tournament_matchup (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    phase_id INTEGER NOT NULL,
+    song_1 INTEGER NOT NULL,
+    song_2 INTEGER,
+    song_winner INTEGER,
+    FOREIGN KEY (phase_id) REFERENCES tournament_phase (id),
+    FOREIGN KEY (song_1) REFERENCES song (id),
+    FOREIGN KEY (song_2) REFERENCES song (id),
+    FOREIGN KEY (song_winner) REFERENCES song (id)
+);
