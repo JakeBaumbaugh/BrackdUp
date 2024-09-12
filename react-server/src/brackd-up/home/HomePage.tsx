a3543845f637ecb3685ca3816cb6a1f417fca132
JakeBaumbaugh <jake.baumbaugh@gmail.com>
2024-09-11 21:57:50 -0400

    scrollable homepage categories

--- react-server/src/brackd-up/home/HomePage.tsx
+++ react-server/src/brackd-up/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import TournamentSummary from "../../model/TournamentSummary";
 import { getTournaments } from "../../service/TournamentService";
 import { useLoadingScreenContext } from "../../context/LoadingScreenContext";
@@ -53,17 +53,60 @@ export default function HomePage() {
         }).then(() => setLoading(false));
 
     return <div className="home-page">
-        {Object.entries(collection).map(([category, tournaments]) => (
-            <div className="category" key={category}>
-                <h3>{category}</h3>
-                <div className="tournament-list">
-                    {tournaments ? tournaments.map(summary => (
-                        <TournamentCard summary={summary} key={summary.id}/>
-                    )) : (
-                        <p>No tournaments found.</p>
-                    )}
-                </div>
-            </div>
+        {Object.entries(collection).map(([category, summaries]) => (
+            <Category category={category} summaries={summaries} key={category}/>
         ))}
+    </div>
+}
+
+interface CategoryProps {
+    category: string;
+    summaries?: TournamentSummary[];
+}
+
+function Category({category, summaries}: Readonly<CategoryProps>) {
+    const categoryRef = useRef<HTMLDivElement>(null);
+    const listRef = useRef<HTMLDivElement>(null);
+    
+    const categoryWidth = useMemo(() => {
+        if (!categoryRef.current) {
+            return 0;
+        }
+        const style = getComputedStyle(categoryRef.current);
+        const paddingWidth = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight);
+        const borderWidth = parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth);
+        return parseFloat(style.width) - paddingWidth - borderWidth;
+    }, [categoryRef.current]);
+    const listWidth = useMemo(() => listRef.current?.offsetWidth ?? 0, [listRef.current]);
+    const maxOffset = Math.max(listWidth - categoryWidth, 0);
+
+    const [listOffset, setListOffset] = useState(0);
+    const addListOffset = (step: number) => setListOffset(listOffset => Math.max(Math.min(listOffset + step, maxOffset), 0));
+
+    return <div className="category" ref={categoryRef}>
+        <h3>{category}</h3>
+        <div className="tournament-list" ref={listRef} style={{transform: `translateX(-${listOffset}px)`}}>
+            {summaries ? summaries.map(summary => (
+                <TournamentCard summary={summary} key={summary.id}/>
+            )) : (
+                <p>No tournaments found.</p>
+            )}
+        </div>
+        {listOffset > 0 && <ListScrollButton mode="left" onClick={() => addListOffset(-500)}/>}
+        {listOffset < maxOffset && <ListScrollButton mode="right" onClick={() => addListOffset(500)}/>} 
+    </div>
+}
+
+interface ListScrollButtonProps {
+    mode: "left"|"right";
+    onClick: () => void;
+}
+
+function ListScrollButton({mode, onClick}: Readonly<ListScrollButtonProps>) {
+    const className = `scroll-button ${mode}-scroll-button`;
+    const text = mode === "left" ? "<" : ">";
+
+    return <div className={className} onClick={onClick} role="button">
+        <span>{text}</span>
     </div>
 }
